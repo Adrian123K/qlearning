f24aa487e10e52fef3804e00f71c3d41561df20e
FatherA <seojun.a.kim@gmail.com>
2020-09-14 13:46:35 +0900

    add files

---
+++ env_const.py
@@ -0,0 +1,60 @@
+from environment import GraphicDisplay
+
+import tkinter as tk
+from tkinter import Button
+import time
+import numpy as np
+from PIL import ImageTk, Image
+
+PhotoImage = ImageTk.PhotoImage
+UNIT = 100  # 픽셀 수a
+HEIGHT = 5  # 그리드월드 세로
+WIDTH = 5  # 그리드월드 가로
+TRANSITION_PROB = 1
+POSSIBLE_ACTIONS = [0, 1, 2, 3]  # 좌, 우, 상, 하
+ACTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 좌표로 나타낸 행동
+REWARDS = []
+
+
+class Env:
+    def __init__(self):
+        self.transition_probability = TRANSITION_PROB
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.reward = [[0] * WIDTH for _ in range(HEIGHT)]
+        self.possible_actions = POSSIBLE_ACTIONS
+        self.reward[2][2] = 1  # (2,2) 좌표 동그라미 위치에 보상 1
+        self.reward[1][2] = -1  # (1,2) 좌표 세모 위치에 보상 -1
+        self.reward[2][1] = -1  # (2,1) 좌표 세모 위치에 보상 -1
+        self.all_state = []
+
+        for x in range(WIDTH):
+            for y in range(HEIGHT):
+                state = [x, y]
+                self.all_state.append(state)
+
+    def get_reward(self, state, action):
+        next_state = self.state_after_action(state, action)
+        return self.reward[next_state[0]][next_state[1]]
+
+    def state_after_action(self, state, action_index):
+        action = ACTIONS[action_index]
+        return self.check_boundary([state[0] + action[0], state[1] + action[1]])
+
+    @staticmethod
+    def check_boundary(state):
+        state[0] = (0 if state[0] < 0 else WIDTH - 1
+                    if state[0] > WIDTH - 1 else state[0])
+        state[1] = (0 if state[1] < 0 else HEIGHT - 1
+                    if state[1] > HEIGHT - 1 else state[1])
+        return state
+
+    def get_transition_prob(self, state, action):
+        return self.transition_probability
+
+    def get_all_states(self):
+        return self.all_state
+
+env = Env()
+
+print(env.get_all_states())
